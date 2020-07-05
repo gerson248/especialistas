5bc233d95c1e525d9e1b5837736fc67815d94091
luisricardo123 <58831965+luisricardo123@users.noreply.github.com>
2020-07-05 03:34:24 -0500

    Agregando DataTables, Formas, Pestañas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onscroll = function(){ //Percibe si haces scroll
-    if(document.documentElement.scrollTop> 150){
+    if(document.documentElement.scrollTop> 30){
         document.getElementById("scroll-hacia-arriba").style.display="initial"; //Vuelve al estilo de display inicial
     }else{
         document.getElementById("scroll-hacia-arriba").style.display="none"; //Pone el estilo de display a "none"
@@ -9,3 +9,77 @@ window.onscroll = function(){ //Percibe si haces scroll
 document.getElementById("scroll-hacia-arriba").onclick = function(){
     document.documentElement.scrollTop = 0; //Posiciona el scroll, El numero es la distancia desde la parte superior de la pagina hacia abajo
 }    
+
+$(document).ready(function() {
+    $('#TablaConsorcio').DataTable({
+        "language":{
+            "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+            }
+        },
+        "columnDefs":[
+            {"orderable" :false, "targets": [8,10,11]},
+        ],
+        "lengthMenu": [3,5,10,15]
+        
+        
+        
+    })
+    $('#TablaCliente').DataTable({
+        "language":{
+            "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            },
+            "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+            }
+        },
+        "columnDefs":[
+            {"orderable" :false, "targets": [8,10]},
+        ],
+        "lengthMenu": [3,5,10,15]
+    })
+}); 
