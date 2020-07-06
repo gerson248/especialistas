56f8e415d92bbf1775944f58b64530ff23c74c21
luisricardo123 <58831965+luisricardo123@users.noreply.github.com>
2020-07-06 00:40:14 -0500

    Cambios de DataTables y agregando pestañas

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-window.onscroll = function(){ //Percibe si haces scroll
-    if(document.documentElement.scrollTop> 30){
-        document.getElementById("scroll-hacia-arriba").style.display="initial"; //Vuelve al estilo de display inicial
+//Funcion que hace que al hacer click al boton de scroll se haga un desplazamiento hacia arriba con una duracion de animacion determinada.
+$("#scroll-hacia-arriba").click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 300);
+    return false;
+});
+
+//Funcion que hace mostrar u ocultar el boton de scroll dependiendo de la altura de la pagina en el que el usuario se encuentra.
+$(window).on("scroll",function(){
+    if($(this).scrollTop() > 100){
+        $("#scroll-hacia-arriba").fadeIn(); 
     }else{
-        document.getElementById("scroll-hacia-arriba").style.display="none"; //Pone el estilo de display a "none"
+        $("#scroll-hacia-arriba").fadeOut(); 
     }
-}
+})
 
-document.getElementById("scroll-hacia-arriba").onclick = function(){
-    document.documentElement.scrollTop = 0; //Posiciona el scroll, El numero es la distancia desde la parte superior de la pagina hacia abajo
-}    
-
+// Acciones o funciones que se realizaran cuando la pagina se recargue.
 $(document).ready(function() {
-    $('#TablaConsorcio').DataTable({
+    //Aplicando la herramienta DataTables a la tabla Personal del Consorcio
+    $('#TablaConsorcio').DataTable({  
+        //Cambio de lenguaje a español
         "language":{
             "sProcessing":     "Procesando...",
             "sLengthMenu":     "Mostrar _MENU_ registros",
@@ -40,15 +46,18 @@ $(document).ready(function() {
                 "colvis": "Visibilidad"
             }
         },
-        "columnDefs":[
-            {"orderable" :false, "targets": [8,10,11]},
-        ],
-        "lengthMenu": [3,5,10,15]
+        
+        "columnDefs":[{"orderable" :false,"targets": [4,5,8,10,11]}], //Elimina las flechas de ordenar en las columnas en los [].
+        "lengthMenu": [3,5,10,15], //Establece los valores del select del numero de filas que se quiere mostrar.
+        "info": false //Elimina la informacion de paginas mostradas de la parte inferior de la tabla.
+
         
         
         
-    })
-    $('#TablaCliente').DataTable({
+    });
+
+    //Aplicando la herramienta DataTables a la tabla Especialistas del cliente
+    $('#TablaCliente').DataTable({ 
         "language":{
             "sProcessing":     "Procesando...",
             "sLengthMenu":     "Mostrar _MENU_ registros",
@@ -77,9 +86,17 @@ $(document).ready(function() {
                 "colvis": "Visibilidad"
             }
         },
-        "columnDefs":[
-            {"orderable" :false, "targets": [8,10]},
-        ],
-        "lengthMenu": [3,5,10,15]
-    })
-}); 
+
+        "columnDefs":[{"orderable" :false, "targets": [4,5,8,9,10]},], //Elimina las flechas de ordenar en las columnas en los [].
+        "lengthMenu": [3,5,10,15], //Establece los valores del select del numero de filas que se quiere mostrar
+        "info": false, //Elimina la informacion de paginas mostradas de la parte inferior de la tabla.
+    });
+
+    //Mostrar u ocultar el boton de scroll dependiendo de la altura de la pagina en el que el usuario se encuentra.
+    if($(window).scrollTop() > 100){
+        $("#scroll-hacia-arriba").fadeIn(); 
+    }else{
+        $("#scroll-hacia-arriba").fadeOut(); 
+    };
+
+});
