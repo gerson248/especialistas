0a5f2368c7f60c1682f7dc7bd9912df4d367e79d
Enzo <malstro4@hotmail.es>
2020-07-25 21:03:53 -0500

    Pintando los proyectos en la base de datos
    Los proyectos en la base de datos ya están renderizados
    y se muestran, ahora falta que cuando haga click muestre
    su data asociada.

--- empresa/posts/views.py
+++ empresa/posts/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-	return render(request,'posts/index.html',)
+from .models import Proyecto, Personal_Cliente
+from .models import Consorcio, Personal_Consorcio
+
+class IndexView(generic.ListView):
+    template_name  = 'posts/index.html'
+    context_object_name = 'projects'
+    def get_queryset(self):
+        """Retorna todos los proyectos registrados hasta ahora"""
+        return Proyecto.objects.all()
